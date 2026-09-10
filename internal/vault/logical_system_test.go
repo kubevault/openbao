df8db5208cdbfb3760ec485c3f754b3970db8bfc
Abdullah Al Masood <masood@appscode.com>
2026-09-10 15:32:57 +0600

    Fix Neo4j plugin registry test fixtures
    Signed-off-by: Abdullah Al Masood <masood@appscode.com>

--- internal/vault/logical_system_test.go
+++ internal/vault/logical_system_test.go
@@ -3886,9 +3886,9 @@ func TestSystemBackend_PluginCatalog_List(t *testing.T) {
 			"keys": []string{
 				"cassandra-database-plugin", "elasticsearch-database-plugin", "influxdb-database-plugin", "mongodb-database-plugin", "mssql-database-plugin", "mysql-aurora-database-plugin",
 				"mysql-database-plugin", "mysql-legacy-database-plugin", "mysql-rds-database-plugin",
-				"oracle-database-plugin", "neo4j-database-plugin", "postgresql-database-plugin", "rabbitmq-database-plugin", "redis-database-plugin",
+				"neo4j-database-plugin", "oracle-database-plugin", "postgresql-database-plugin", "rabbitmq-database-plugin", "redis-database-plugin",
 				"remote-cassandra-plugin", "remote-elasticsearch-plugin", "remote-influxdb-plugin", "remote-mongodb-plugin", "remote-mssql-plugin", "remote-mysql-plugin",
-				"remote-oracle-plugin", "remote-neo4j-plugin", "remote-postgres-plugin", "remote-rabbitmq-plugin", "remote-redis-plugin", "remote-valkey-plugin",
+				"remote-neo4j-plugin", "remote-oracle-plugin", "remote-postgres-plugin", "remote-rabbitmq-plugin", "remote-redis-plugin", "remote-valkey-plugin",
 				"test-plugin", "valkey-database-plugin",
 			},
 		}); diff != nil {
@@ -3917,9 +3917,9 @@ func TestSystemBackend_PluginCatalog_List(t *testing.T) {
 			"database": []string{
 				"cassandra-database-plugin", "elasticsearch-database-plugin", "influxdb-database-plugin", "mongodb-database-plugin", "mssql-database-plugin", "mysql-aurora-database-plugin",
 				"mysql-database-plugin", "mysql-legacy-database-plugin", "mysql-rds-database-plugin",
-				"oracle-database-plugin", "neo4j-database-plugin", "postgresql-database-plugin", "rabbitmq-database-plugin", "redis-database-plugin",
+				"neo4j-database-plugin", "oracle-database-plugin", "postgresql-database-plugin", "rabbitmq-database-plugin", "redis-database-plugin",
 				"remote-cassandra-plugin", "remote-elasticsearch-plugin", "remote-influxdb-plugin", "remote-mongodb-plugin", "remote-mssql-plugin", "remote-mysql-plugin",
-				"remote-oracle-plugin", "remote-neo4j-plugin", "remote-postgres-plugin", "remote-rabbitmq-plugin", "remote-redis-plugin", "remote-valkey-plugin",
+				"remote-neo4j-plugin", "remote-oracle-plugin", "remote-postgres-plugin", "remote-rabbitmq-plugin", "remote-redis-plugin", "remote-valkey-plugin",
 				"test-plugin", "valkey-database-plugin",
 			},
 			"detailed": []map[string]any{{
@@ -3989,13 +3989,13 @@ func TestSystemBackend_PluginCatalog_List(t *testing.T) {
 				"type":               "database",
 				"version":            "v2.0.0+builtin.bao",
 			}, {
-				"name":               "oracle-database-plugin",
+				"name":               "neo4j-database-plugin",
 				"builtin":            true,
 				"deprecation_status": "supported",
 				"type":               "database",
 				"version":            "v2.0.0+builtin.bao",
 			}, {
-				"name":               "neo4j-database-plugin",
+				"name":               "oracle-database-plugin",
 				"builtin":            true,
 				"deprecation_status": "supported",
 				"type":               "database",
@@ -4055,13 +4055,13 @@ func TestSystemBackend_PluginCatalog_List(t *testing.T) {
 				"type":               "database",
 				"version":            "v2.0.0+builtin.bao",
 			}, {
-				"name":               "remote-oracle-plugin",
+				"name":               "remote-neo4j-plugin",
 				"builtin":            true,
 				"deprecation_status": "supported",
 				"type":               "database",
 				"version":            "v2.0.0+builtin.bao",
 			}, {
-				"name":               "remote-neo4j-plugin",
+				"name":               "remote-oracle-plugin",
 				"builtin":            true,
 				"deprecation_status": "supported",
 				"type":               "database",
